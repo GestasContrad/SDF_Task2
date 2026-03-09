055218e8c057219ea676d2f3f0a34f3e698eb987
Асель Абдрахманова <dellya4>
2026-03-10 00:32:38 +0500

    refactor: separate parse and load function

--- src/main/java/org/example/model/Task.java
+++ src/main/java/org/example/model/Task.java
@@ -26,6 +26,7 @@ public class Task {
         return completed ? "Completed" : "Pending";
     }
 
+    @Override
     public String toString() {
         return id + ". " + title + " (" + description + ") - " + getStatusText();
     }
--- src/main/java/org/example/storage/FileStorage.java
+++ src/main/java/org/example/storage/FileStorage.java
@@ -24,6 +24,17 @@ public class FileStorage {
         }
     }
 
+    public static Task parseTask(String line) {
+        String[] parts = line.split(DELIMITER);
+
+        int id = Integer.parseInt(parts[0]);
+        String title = parts[1];
+        String description = parts[2];
+        boolean completed = Boolean.parseBoolean(parts[3]);
+
+        return new Task(id, title, description, completed);
+    }
+
     public static List<Task> loadTasks() {
         List<Task> tasks = new ArrayList<>();
         File file = new File(FILE_NAME);
@@ -35,14 +46,8 @@ public class FileStorage {
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(DELIMITER);
-                if (parts.length == 4) {
-                    int id = Integer.parseInt(parts[0]);
-                    String title = parts[1];
-                    String description = parts[2];
-                    boolean isCompleted = Boolean.parseBoolean(parts[3]);
-
-                    tasks.add(new Task(id, title, description, isCompleted));
+                if (!line.isBlank()) {
+                    tasks.add(parseTask(line));
                 }
             }
         } catch (IOException | NumberFormatException e) {
